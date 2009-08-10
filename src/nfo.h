a3bcff39374328b93d28e0cdeccb317a27629c8c
Fabien Brisset <fbrisset@gmail.com>
2009-08-10 22:23:23 +0200

    definition of structures and functions

--- src/nfo.h
+++ src/nfo.h
@@ -62,4 +62,74 @@ extern "C" {
 }
 #endif /* __cplusplus */
 
+/* Structure definitions */
+typedef struct nfo_actor_s nfo_actor_t;
+typedef struct nfo_movie_s nfo_movie_t;
+typedef struct nfo_tvshow_s nfo_tvshow_t;
+typedef struct nfo_tvshow_episode_s nfo_tvshow_episode_t;
+typedef enum nfo_field_s nfo_field_t;
+typedef enum nfo_type_s nfo_type_t;
+
+enum nfo_type_s
+{
+    NFO_MOVIE,
+    NFO_TVSHOW,
+    NFO_TVSHOW_EPISODE,
+    NFO_ACTOR
+};
+
+enum nfo_field_s
+{
+   NFO_ACTOR_NAME,
+   NFO_ACTOR_ROLE,
+   NFO_TITLE,
+   NFO_ORIGINAL_TITLE,
+   NFO_RATING,
+   NFO_YEAR,
+   NFO_TOP250,
+   NFO_VOTES,
+   NFO_OUTLINE,
+   NFO_PLOT,
+   NFO_TAGLINE,
+   NFO_RUNTIME,
+   NFO_THUMB,
+   NFO_MPAA,
+   NFO_PLAYCOUNT,
+   NFO_WATCHED,
+   NFO_ID,
+   NFO_FILE_PATH,
+   NFO_TRAILER,
+   NFO_GENRE,
+   NFO_CREDITS,
+   NFO_DIRECTOR,
+   NFO_EPISODE_GUIDE_URL,
+   NFO_SEASON,
+   NFO_EPISODE,
+   NFO_DISPLAYSEASON,
+   NFO_DISPLAYEPISODE,
+   NFO_TVDBID,
+   NFO_PREMIERED,
+   NFO_STUDIO,
+   NFO_SORTORDER,
+   NFO_LANGUAGE,
+   NFO_EPISODE_ACTOR_SOURCE,
+   NFO_AIRED
+};
+
+/* Get Methods */
+void nfo_actor_get(nfo_actor_t *nfo_actor, nfo_field_t type, char *value);
+void nfo_get(nfo_type_t nfo_type, nfo_field_t type, char *value);
+void nfo_get_actors(nfo_type_t nfo_type, nfo_actor_t *value);
+
+/* Set Methods */
+void nfo_actor_set(nfo_actor_t *nfo_actor, nfo_field_t type, char *value);
+void nfo_set(nfo_type_t nfo_type, nfo_field_t type, char *value);
+void nfo_set_actors(nfo_type_t nfo_type, nfo_actor_t *value);
+
+void nfo_tvshow_add_episode();
+void nfo_tvshow_remove_episode();
+
+void nfo_tvshow_add_actor();
+void nfo_tvshow_remove_actor();
+
 #endif /* NFO_H */
