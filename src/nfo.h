7429abff4614050fea5b69b0793ec93cb2f93f76
Benjamin Zores <ben@geexbox.org>
2009-09-25 20:53:24 +0200

    complete rework of public api

--- src/nfo.h
+++ src/nfo.h
@@ -71,58 +71,155 @@ typedef struct nfo_actor_s nfo_actor_t;
 typedef struct nfo_movie_s nfo_movie_t;
 typedef struct nfo_tvshow_s nfo_tvshow_t;
 typedef struct nfo_tvshow_episode_s nfo_tvshow_episode_t;
-typedef enum nfo_field_s nfo_field_t;
-typedef enum nfo_type_s nfo_type_t;
+typedef struct nfo_s nfo_t;
 
-enum nfo_type_s
+typedef enum nfo_type_s
 {
-    NFO_MOVIE,
-    NFO_TVSHOW,
-    NFO_TVSHOW_EPISODE,
-    NFO_ACTOR
-};
+  NFO_UNKNOWN,
+  NFO_MOVIE,
+  NFO_TVSHOW,
+  NFO_MUSIC,
+} nfo_type_t;
 
-enum nfo_field_s
-{
-   NFO_ACTOR_NAME,
-   NFO_ACTOR_ROLE,
-   NFO_TITLE,
-   NFO_ORIGINAL_TITLE,
-   NFO_RATING,
-   NFO_YEAR,
-   NFO_TOP250,
-   NFO_VOTES,
-   NFO_OUTLINE,
-   NFO_PLOT,
-   NFO_TAGLINE,
-   NFO_RUNTIME,
-   NFO_THUMB,
-   NFO_MPAA,
-   NFO_PLAYCOUNT,
-   NFO_WATCHED,
-   NFO_ID,
-   NFO_FILE_PATH,
-   NFO_TRAILER,
-   NFO_GENRE,
-   NFO_CREDITS,
-   NFO_DIRECTOR,
-   NFO_EPISODE_GUIDE_URL,
-   NFO_SEASON,
-   NFO_EPISODE,
-   NFO_DISPLAYSEASON,
-   NFO_DISPLAYEPISODE,
-   NFO_TVDBID,
-   NFO_PREMIERED,
-   NFO_STUDIO,
-   NFO_SORTORDER,
-   NFO_LANGUAGE,
-   NFO_EPISODE_ACTOR_SOURCE,
-   NFO_AIRED
-};
+typedef enum nfo_actor_field_s {
+  NFO_ACTOR_NAME,
+  NFO_ACTOR_ROLE,
+  NFO_ACTOR_THUMB,
+} nfo_actor_field_t;
 
-/* Get Methods */
-void nfo_actor_get(nfo_actor_t *nfo_actor, nfo_field_t type, char *value);
-void nfo_get(nfo_type_t nfo_type, nfo_field_t type, char *value);
-void nfo_get_actors(nfo_type_t nfo_type, nfo_actor_t *value);
+typedef enum nfo_video_field_s {
+  NFO_VIDEO_WIDTH,
+  NFO_VIDEO_HEIGHT,
+  NFO_VIDEO_CODEC,
+  NFO_VIDEO_FORMAT_INFO,
+  NFO_VIDEO_DURATION,
+  NFO_VIDEO_BITRATE,
+  NFO_VIDEO_BITRATE_MODE,
+  NFO_VIDEO_BITRATE_MAX,
+  NFO_VIDEO_CONTAINER,
+  NFO_VIDEO_CODEC_ID,
+  NFO_VIDEO_CODEC_INFO,
+  NFO_VIDEO_SCAN_TYPE,
+  NFO_VIDEO_ASPECT,
+} nfo_video_field_t;
+
+typedef enum nfo_audio_field_s {
+  NFO_AUDIO_LANG,
+  NFO_AUDIO_CODEC,
+  NFO_AUDIO_CHANNELS,
+  NFO_AUDIO_BITRATE,
+} nfo_audio_field_t;
+
+typedef enum nfo_sub_field_s {
+  NFO_SUB_LANG,
+} nfo_sub_field_t;
+
+typedef enum nfo_movie_field_s {
+  NFO_MOVIE_TITLE,
+  NFO_MOVIE_ORIGINAL_TITLE,
+  NFO_MOVIE_RATING,
+  NFO_MOVIE_YEAR,
+  NFO_MOVIE_TOP250,
+  NFO_MOVIE_VOTES,
+  NFO_MOVIE_OUTLINE,
+  NFO_MOVIE_PLOT,
+  NFO_MOVIE_TAGLINE,
+  NFO_MOVIE_RUNTIME,
+  NFO_MOVIE_THUMB,
+  NFO_MOVIE_FAN_ART,
+  NFO_MOVIE_MPAA,
+  NFO_MOVIE_PLAY_COUNT,
+  NFO_MOVIE_WATCHED,
+  NFO_MOVIE_ID,
+  NFO_MOVIE_TRAILER,
+  NFO_MOVIE_GENRE,
+  NFO_MOVIE_CREDITS,
+  NFO_MOVIE_DIRECTOR,
+  NFO_MOVIE_STUDIO,
+} nfo_movie_field_t;
+
+typedef enum nfo_tvshow_field_s {
+  NFO_TVSHOW_TITLE,
+  NFO_TVSHOW_RATING,
+  NFO_TVSHOW_SEASON,
+  NFO_TVSHOW_EPISODE,
+  NFO_TVSHOW_DISPLAY_SEASON,
+  NFO_TVSHOW_DISPLAY_EPISODE,
+  NFO_TVSHOW_EPISODE_GUIDE_URL,
+  NFO_TVSHOW_PLOT,
+  NFO_TVSHOW_MPAA,
+  NFO_TVSHOW_WATCHED,
+  NFO_TVSHOW_GENRE,
+  NFO_TVSHOW_PREMIERED,
+  NFO_TVSHOW_STUDIO,
+} nfo_tvshow_field_t;
+
+typedef enum nfo_tvshow_episode_field_s {
+  NFO_TVSHOW_EPISODE_TITLE,
+  NFO_TVSHOW_EPISODE_RATING,
+  NFO_TVSHOW_EPISODE_SEASON,
+  NFO_TVSHOW_EPISODE_EPISODE,
+  NFO_TVSHOW_EPISODE_PLOT,
+  NFO_TVSHOW_EPISODE_THUMB,
+  NFO_TVSHOW_EPISODE_PLAY_COUNT,
+  NFO_TVSHOW_EPISODE_CREDITS,
+  NFO_TVSHOW_EPISODE_DIRECTOR,
+  NFO_TVSHOW_EPISODE_AIRED,
+  NFO_TVSHOW_EPISODE_VOTES,
+} nfo_tvshow_episode_field_t;
+
+/* (Un)Init Routines */
+nfo_t *nfo_init (const char *filename);
+void nfo_free (nfo_t *nfo);
+
+/* NFO Get Methods */
+nfo_type_t nfo_get_type (nfo_t *nfo);
+nfo_movie_t *nfo_get_movie (nfo_t *nfo);
+nfo_tvshow_episode_t *nfo_get_tvshow_episode (nfo_t *nfo);
+
+/* Movie Get Methods */
+char *nfo_movie_get (nfo_movie_t *movie, nfo_movie_field_t f);
+
+int nfo_movie_get_actors_count (nfo_movie_t *movie);
+nfo_actor_t *nfo_movie_get_actor (nfo_movie_t *movie, int id);
+
+int nfo_movie_get_video_streams_count (nfo_movie_t *movie);
+nfo_stream_video_t *nfo_movie_get_video_stream (nfo_movie_t *movie, int id);
+
+int nfo_movie_get_audio_streams_count (nfo_movie_t *movie);
+nfo_stream_audio_t *nfo_movie_get_audio_stream (nfo_movie_t *movie, int id);
+
+int nfo_movie_get_sub_streams_count (nfo_movie_t *movie);
+nfo_stream_sub_t *nfo_movie_get_sub_stream (nfo_movie_t *movie, int id);
+
+/* TVShow Get Methods */
+char *nfo_tvshow_episode_get (nfo_tvshow_episode_t *ep,
+                              nfo_tvshow_episode_field_t f);
+
+int nfo_tvshow_episode_get_actors_count (nfo_tvshow_episode_t *ep);
+nfo_actor_t *nfo_tvshow_episode_get_actor (nfo_tvshow_episode_t *ep, int id);
+
+int nfo_tvshow_episode_get_video_streams_count (nfo_tvshow_episode_t *ep);
+nfo_stream_video_t *nfo_tvshow_episode_get_video_stream
+ (nfo_tvshow_episode_t *ep, int id);
+
+int nfo_tvshow_episode_get_audio_streams_count (nfo_tvshow_episode_t *ep);
+nfo_stream_audio_t *nfo_tvshow_episode_get_audio_stream
+ (nfo_tvshow_episode_t *ep, int id);
+
+int nfo_tvshow_episode_get_sub_streams_count (nfo_tvshow_episode_t *ep);
+nfo_stream_sub_t *nfo_tvshow_episode_get_sub_stream
+ (nfo_tvshow_episode_t *ep, int id);
+
+nfo_tvshow_t *nfo_tvshow_episode_get_show (nfo_tvshow_episode_t *ep);
+char *nfo_tvshow_get (nfo_tvshow_t *ep, nfo_tvshow_field_t f);
+
+/* Actor Get Methods */
+char *nfo_actor_get (nfo_actor_t *actor, nfo_actor_field_t f);
+
+/* Streams Get Methods */
+char *nfo_video_stream_get (nfo_stream_video_t *video, nfo_video_field_t f);
+char *nfo_audio_stream_get (nfo_stream_audio_t *video, nfo_audio_field_t f);
+char *nfo_sub_stream_get (nfo_stream_sub_t *video, nfo_sub_field_t f);
 
 #endif /* NFO_H */
